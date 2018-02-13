e02428dc648448191dd443822ce086cecfcc4faf
Balazs Pentek <myrrdhinn@ip-172-31-40-88.eu-central-1.compute.internal>
2018-02-13 13:05:58 +0000

    Search updated

--- resources/assets/src/Components/Global/Header/Search/Search.js
+++ resources/assets/src/Components/Global/Header/Search/Search.js
@@ -4,6 +4,7 @@ const Search = props => {
 
  	return(
 		<section className="Search">
+			<button onClick={props.toggle}></button>
 			<div className={props.addClass ? 'Search--wrp show' : 'Search--wrp' }>
 				<form id="Search--form" className="Search--form">
 					<div className="Search--input-and-results">
@@ -15,7 +16,6 @@ const Search = props => {
 							<p>Stewart Brand: The Long Now</p>
 						</div>
 					</div>
-					<button onClick={props.toggle}></button>
 				</form>
 			</div>
 		</section>
